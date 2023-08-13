ea1af5a0ced28f308cf4321d64099f837f97b2ae
Moayyad Saleh <moayyadalazzam@gmail.com>
2023-08-13 16:06:15 -0600

    add APi GET REQUEST ROUTE

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public')); // Serve static files (CSS, JS)
+app.set('view engine', 'ejs');
 
 const uri = 'mongodb://127.0.0.1:27017/wikiDB';
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -29,20 +30,26 @@ db.on('disconnected', () => {
     console.log('Mongoose disconnected');
 });
 
-// Define schema and model
+// Define schema 
 const articleSchema = new mongoose.Schema({
     title: String,
     content: String
 });
-
+// Define model
 const Article = mongoose.model('Article', articleSchema );
 
 
-
-
-
-
-
+app.get("/articles", (req, res) => {
+    Article.find()
+        .then(foundArticles => {
+            console.log(foundArticles);
+            res.send(foundArticles)
+        })
+        .catch(err => {
+            console.log("Error fetching articles", err);
+            res.status(500).send("Error fetching articles");
+        });
+});
 
 
 

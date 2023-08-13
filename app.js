5469d270bb6584c57c4f2cefd6d6aae482dc98cc
Moayyad Saleh <moayyadalazzam@gmail.com>
2023-08-13 17:14:23 -0600

    refactor code to change HTTP requests

--- app.js
+++ app.js
@@ -39,52 +39,50 @@ const articleSchema = new mongoose.Schema({
 const Article = mongoose.model('Article', articleSchema );
 
 
-app.get("/articles", (req, res) => {
-    Article.find()
-        .then(foundArticles => {
-            console.log(foundArticles);
-            res.send(foundArticles)
-        })
-        .catch(err => {
-            console.log("Error fetching articles", err);
-            res.status(500).send("Error fetching articles");
+
+
+app.route("/articles")
+    .get((req, res) => {
+        Article.find()
+            .then(foundArticles => {
+                console.log(foundArticles);
+                res.send(foundArticles);
+            })
+            .catch(err => {
+                console.log("Error fetching articles", err);
+                res.status(500).send("Error fetching articles");
+            });
+    })
+    .post((req, res) => {
+        console.log(req.body.title);
+        console.log(req.body.content);
+        
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
         });
-});
-
-
-
-app.post("/articles", (req, res) => {
-    console.log(req.body.title);
-    console.log(req.body.content);
-    
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
+        
+        newArticle.save()
+            .then(savedArticle => {
+                console.log("Article saved successfully:", savedArticle);
+                res.status(201).send("Article saved successfully");
+            })
+            .catch(err => {
+                console.log("Error saving article:", err);
+                res.status(500).send("Error saving article");
+            });
+    })
+    .delete((req, res) => {
+        Article.deleteMany()
+            .then(deletedArticles => {
+                console.log(deletedArticles, "Articles are successfully deleted");
+                res.send(deletedArticles);
+            })
+            .catch(err => {
+                console.log("Error deleting articles", err);
+                res.status(500).send("Error deleting articles");
+            });
     });
-    
-    newArticle.save()
-        .then(savedArticle => {
-            console.log("Article saved successfully:", savedArticle);
-            res.status(201).send("Article saved successfully");
-        })
-        .catch(err => {
-            console.log("Error saving article:", err);
-            res.status(500).send("Error saving article");
-        });
-});
-
-
-app.delete("/articles", (req, res) => {
-    Article.deleteMany()
-        .then(deletedArticles => {
-            console.log(deletedArticles, "Articles are succefully delted");
-            res.send(deletedArticles);
-        })
-        .catch(err => {
-            console.log("Error deleting articles", err);
-            res.status(500).send("Error deleting articles");
-        });
-});
 
 
 app.listen(PORT, () => {
